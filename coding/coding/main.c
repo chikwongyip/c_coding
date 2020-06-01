fc90d2046ddce252ff2214dd1b7cd4c09193265a
chikwongyip <527439024@qq.com>
2020-06-01 22:03:15 +0800

    test for commit for xcode

---
+++ coding/coding/main.c
@@ -0,0 +1,17 @@
+//
+//  main.c
+//  coding
+//
+//  Created by ChiKwong Yip on 1/6/2020.
+//  Copyright © 2020 ChiKwong Yip. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    char ch;
+    printf("Please enter a charater.\n");
+    scanf("%c",&ch);
+    printf("The code for %c is %d.\n",ch,ch);
+    return 0;
+}
