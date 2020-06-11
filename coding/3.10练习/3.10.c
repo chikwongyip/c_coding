7a8956c63c232593e77f0c0d503463c6f6e34cf1
chikwongyip <527439024@qq.com>
2020-06-11 22:10:21 +0800

    test

---
+++ coding/3.10练习/3.10.c
@@ -0,0 +1,23 @@
+//
+//  3.10.c
+//  coding
+//
+//  Created by ChiKwong Yip on 11/6/2020.
+//  Copyright © 2020 ChiKwong Yip. All rights reserved.
+//
+
+#include <stdio.h>
+int test(void)
+{
+//    east simpleton 人口
+    int poeplation;
+//    DVD 价格
+    float price;
+//   本章出现次数最多的字母
+    char c;
+//    本章出现次数最多字母的次数
+    int times;
+//    在什么情况下要用long类型的变量替代int类型的变量？
+    
+    return 0;
+}
